af9e89e8058c09cfadc97e446548f0dcfd78de53
Jared Frank <jfrank1120@verizon.net>
2020-12-19 02:20:26 -0500

    Worked on database work for getting players from the game

--- game.py
+++ game.py
@@ -3,10 +3,12 @@ import string
 
 
 class Game(object):
-    def __init__(self, hostname):
+    def __init__(self, hostname, players=None, game_code=None, current_card=None, num_players=None):
+        self.game_code = game_code
+        self.players = players
         self.hostname = hostname
-        self.players = []
-        self.game_code = ""
+        self.num_players = num_players
+        self.current_card = current_card
         for i in range(7):
             if i % 2 == 0:
                 self.game_code = self.game_code + choice(string.ascii_letters)
@@ -14,9 +16,12 @@ class Game(object):
                 self.game_code = self.game_code + str(randint(1, 9))
         print(self.game_code)
 
+
     def toDict(self):
         return {
             'hostname': self.hostname,
             'players': self.players,
-            'game_code': self.game_code
+            'game_code': self.game_code,
+            'num_players': self.num_players,
+            'current_card': self.current_card
         }
--- gameData.py
+++ gameData.py
@@ -2,10 +2,54 @@ from google.cloud import datastore
 from game import Game
 from main import log
 
-def getClient():
-    client = None;
+
+def get_client():
     try:
         client = datastore.Client()
         return client
     except:
         return datastore.Client.from_service_account_json("API_KEY.json")
+
+
+def load_key(client, entity_type, entity_id=None, parent_key=None):
+    key = None
+    if entity_id:
+        key = client.key(entity_type, entity_id, parent_key=parent_key)
+    else:
+        key = client.key(entity_type)
+    return key
+
+
+def load_entity(client, entity_type, entity_id, parent_key=None):
+    key = load_key(client, entity_type, parent_key)
+    entity = client.get(key)
+    log('Retrieved entity for ' + str(entity_id))
+    return entity
+
+
+# Load the Game from DataStore into an object
+def game_from_entity(game_entity):
+    hostname = game_entity['Host']
+    num_players = game_entity['Number of Players']
+    players = game_entity['Players']
+    current_card = game_entity['Current Card']
+    game_code = game_entity['Game Code']
+    game_val = Game(hostname, players, game_code, current_card, num_players)
+    return game_val
+
+
+# Load players in the game based on the Queried Game Code
+def load_players(game_code):
+    log('Loading players for game_code' + str(game_code))
+    client = get_client()
+    query = client.query(kind='Game')
+    query.add_filter('Game Code', '=', game_code)
+    return_list = []
+    iterable = list(query.fetch())
+    log('Num Games: ' + str(len(iterable)))
+    for x in iterable:
+        new_game = game_from_entity(x)
+        print(new_game.players)
+        return_list.append(new_game.players)
+
+    return return_list
