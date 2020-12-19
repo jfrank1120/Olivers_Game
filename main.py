dedf73d824f8d0183f0d7bfd1a948a037894f647
Jared Frank <jfrank1120@verizon.net>
2020-12-18 19:03:27 -0500

    Small Changes

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ def root():
     return redirect("/static/index.html", code=302)
 
 
-@app.route('/user_attempt_login', methods=["POST"])
-def user_attempt_login():
-    print('lol')
+@app.route('/user_join_attempt', methods=["POST"])
+def user_join_attempt():
+    game_code = flask.request['game_code']
+    print('Player Attempting to join game: ' + str(game_code))
+    # Access the Database to see if a game with that code exists
 
 
+# Main Method, Nothing to see here
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
